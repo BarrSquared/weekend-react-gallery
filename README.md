65379f7d2d74bd3a88004a9cc67a5065d69d6093
Russell Barr <rbarrsquared@gmail.com>
2021-09-17 16:36:58 -0500

    added photos and updated photo path in gallery.data.js

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# Project Name
+# Project: Gallery of My Life
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+
 
 ## Description
 
 Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+### Task List
+
+[] Break project down into smaller pieces
+    - commit with each step
+    - keep steps to where you can see updates
+[] Add images to public gallery
+    [] Update gallery.data.js with path for each image
+[] 
--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 2, path: 'images/barr_farm.png', description: 'Photo of a primitive dam from the Barr homestead.', likes: 0 }
+    { id: 3, path: 'images/chuck_the_duck.png', description: 'Photo of my rubber duck, Chuck.', likes: 0 }
+    { id: 4, path: 'images/interesting_wood.png', description: 'Photo of a 2 pieces of wood that I will be making into a board game table with my uncle.', likes: 0 }
+    { id: 5, path: 'images/pygmy_forrest.png', description: 'Photo of a fallen tree in the Pygmy Forest.', likes: 0 }
+    { id: 6, path: 'images/silver_kitty.png', description: 'Photo of my apartment kitty, Silver.', likes: 0 }
 ];
+
 
 module.exports = galleryItems;
