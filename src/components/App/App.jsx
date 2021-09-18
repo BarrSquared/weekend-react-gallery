72dc8ccf51f1e890483043cff3c6a55409d87e09
Russell Barr <rbarrsquared@gmail.com>
2021-09-18 15:21:33 -0500

    images displaying on DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,16 +10,16 @@ function App() {
   //destructuring galleryArray
   const [galleryArray, setGalleryArray] = useState ([]);
 
-  const [galleryItem, setGalleryItem] = useState('');
+  // const [galleryItem, setGalleryItem] = useState('');
 
   const [counter, setCounter] = useState(0);
 
-    const fetchGallery = () => {
+    const fetchGalleryArray = () => {
       axios({
         method: 'GET',
         url: '/gallery',
       }).then((response) => {
-        console.log(response.data);
+        console.log('pulling data from gallery ', response.data);
         setGalleryArray(response.data);
       }).catch((error) => {
         alert('Error making GET request.');
@@ -28,7 +28,7 @@ function App() {
     }
     // to call fetchGallery on load
     useEffect(() => {
-      fetchGallery();
+      fetchGalleryArray();
     }, []);
 
     // function galleryItem({ picture }) {
@@ -49,11 +49,11 @@ function App() {
         <p>Gallery goes here</p>
         <ul>
           <>
-          {galleryArray.map(image => {
-            (<li key={image.path}></li>)
-          })}
-          <div>{ counter } people love it!</div>
-          <button onClick={ handleLikeClick }>Love it!</button>
+          <div>
+            {galleryArray.map((image) => <img key={image.path} src={image.path} />)}
+            <div>{ counter } people love it!</div>
+            <button onClick={ handleLikeClick }>Love it!</button>
+          </div>
           </>
         </ul>
         <img src="images/goat_small.jpg"/>
