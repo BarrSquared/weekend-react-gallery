393cc3ae278daa8a25a53b5f6fa5c44e7dc1d29e
Russell Barr <rbarrsquared@gmail.com>
2021-09-18 15:32:51 -0500

    fixed pygmy_forest, now appearing on DOM

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -3,7 +3,7 @@ const galleryItems = [
     { id: 2, path: 'images/barr_farm.jpg', description: 'Photo of a primitive dam from the Barr homestead.', likes: 0 },
     { id: 3, path: 'images/chuck_the_duck.jpg', description: 'Photo of my rubber duck, Chuck.', likes: 0 },
     { id: 4, path: 'images/interesting_wood.jpg', description: 'Photo of a 2 pieces of wood that I will be making into a board game table with my uncle.', likes: 0 },
-    { id: 5, path: 'images/pygmy_forrest.jpg', description: 'Photo of a fallen tree in the Pygmy Forest.', likes: 0 },
+    { id: 5, path: 'images/pygmy_forest.jpg', description: 'Photo of a fallen tree in the Pygmy Forest.', likes: 0 },
     { id: 6, path: 'images/silver_kitty.jpg', description: 'Photo of my apartment kitty, Silver.', likes: 0 }
 ];
 
